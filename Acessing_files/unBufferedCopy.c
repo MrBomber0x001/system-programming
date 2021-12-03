065542e1a7fbb13bfced77015371c2f5b643b672
Yousef Meska <mrbomberboy123@gmail.com>
2021-12-03 07:42:52 +0200

    Buffered Copy

---
+++ Acessing_files/unBufferedCopy.c
@@ -0,0 +1,31 @@
+#include <fcntl.h>
+#include <stdlib.h>
+
+#define BSIZE 16384
+
+int main()
+{
+    int fin, fout;
+
+    char buf[BSIZE];
+
+    int count;
+
+    if ((fin = open("foo", O_RDONLY)) < 0)
+    {
+        perror("foo");
+        exit(1);
+    }
+    if ((fout = open("bar", O_WRONLY | O_CREAT, 0644)) < 0)
+    {
+        perror("bar");
+        exit(2);
+    }
+
+    while ((count = read(fin, buf, BSIZE)) > 0)
+        write(fout, buf, count);
+
+    close(fin);
+    close(fout);
+    return 0;
+}
